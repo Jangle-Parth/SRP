a949789cdbacbbd72d3bce3f1c8f99734038ab46
Jangle <onlyforparthcoding+github@gmail.com>
2024-12-18 17:14:38 +0530

    changes

--- app.py
+++ app.py
@@ -95,5 +95,8 @@ def upload_image():
 
     return {'error': 'File type not allowed'}, 400
 
+
+    # Dynamically set port
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
